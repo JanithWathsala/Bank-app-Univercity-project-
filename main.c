101a80df3892acc4d2cef0fab2fa409fdd780e3d
Janith Wathsala <janijaniwathsala@gmail.com>
2024-04-25 10:17:16 +0530

    code continue

--- main.c
+++ main.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 
+void mainInterface(){
+    int selec1;
+    printf("HI DEAR");
+    printf("\n\n1.Create account\n2.Login account");
+    printf("\n\nSelect your option:");
+    scanf("%d", &selec1);
+    system("clear || cls");
+
+    if(selec1=1){
+        createAcount();
+    }
+    else if(selec1=2){
+        logAcc();
+    }
+}
+
+void createAcount(){
+    int age,phone;
+    char name,pwd;
+  printf("ACCOUNT CREATE\n\n");
+  printf("Enter your user name:");
+  scanf("%s", &name);
+  printf("Enter new your password:");
+  scanf("%d",&pwd);
+  printf("");
+  printf("Enter phone number:");
+  scanf("%d",&phone);
+}
+
+void logAcc(){
+  printf("WELLOCE!!");
+  printf("Enter account number:");
+  scanf("");
+}
+
  int main() {
-
-     int selec1;
-
-     printf("HI DEAR");
-     printf("\n\n1.Create account\n2.Login account");
-     printf("\n\nSelect your option:");
-     scanf("%d", &selec1);
-
+  mainInterface();
  return 0;
  }
