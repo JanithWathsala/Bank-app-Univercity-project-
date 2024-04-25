4762090cf4b26f542c4fc47eba93b2edb2dd529f
Janith Wathsala <janijaniwathsala@gmail.com>
2024-04-25 08:32:08 +0530

    Main interface create (code write)
    create main interface. 'login'  'and create '  option create.

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
-#includ<stdio.h>
+#include <stdio.h>
 
-int main() {
-  
-return 0;
-}
+ int main() {
+
+     int selec1;
+
+     printf("HI DEAR");
+     printf("\n\n1.Create account\n2.Login account");
+     printf("\n\nSelect your option:");
+     scanf("%d", &selec1);
+
+ return 0;
+ }
